647c895331e222e82952a34a0bba2ef5b4f4ca56
Luca <luca@teapot.ovh>
2024-05-14 12:48:28 +0200

    use baseline_nchw::ternarize

--- src/verify.cpp
+++ src/verify.cpp
@@ -132,20 +132,21 @@ void verify(Registry r) {
       assert(data.real_kernel.dim3 == data.kernel_h);
       assert(data.real_kernel.dim4 == data.kernel_w);
       assert(data.kernel_threshold.size == data.real_kernel.dim1);
-      baseline::ternarize_NCHW_to_NHWCB(
-          data.real_kernel.data, 0, 0, data.kernel_threshold.data,
-          data.kernel_n, data.channels, data.kernel_h, data.kernel_w,
-          data.kernel.data);
-      // bsaeline_nchw::ternarize should be used instead, + the following code
+      // baseline::ternarize_NCHW_to_NHWCB(
+      //     data.real_kernel.data, 0, 0, data.kernel_threshold.data,
+      //     data.kernel_n, data.channels, data.kernel_h, data.kernel_w,
+      //     data.kernel.data);
+      Tensor5D<int64_t> kernel = baseline_nchw::ternarize(
+          data.real_kernel, data.kernel_threshold, 0, 0);
       // Sanity checks on the kernel size, before copying data over
-      // assert(kernel.dim1 == data.kernel.dim1);
-      // assert(kernel.dim2 == data.kernel.dim2);
-      // assert(kernel.dim3 == data.kernel.dim3);
-      // assert(kernel.dim4 == data.kernel.dim4);
-      // assert(kernel.dim5 == data.kernel.dim5);
-      // memcpy(data.kernel.data, kernel.data,
-      //        data.kernel.dim1 * data.kernel.dim2 * data.kernel.dim3 *
-      //            data.kernel.dim4 * data.kernel.dim5);
+      assert(kernel.dim1 == data.kernel.dim1);
+      assert(kernel.dim2 == data.kernel.dim2);
+      assert(kernel.dim3 == data.kernel.dim3);
+      assert(kernel.dim4 == data.kernel.dim4);
+      assert(kernel.dim5 == data.kernel.dim5);
+      memcpy(data.kernel.data, kernel.data,
+             sizeof(int64_t) * data.kernel.dim1 * data.kernel.dim2 *
+                 data.kernel.dim3 * data.kernel.dim4 * data.kernel.dim5);
 
       // NOTE: for when we add binary layers back, we should quantize the
       // weights differently based on the type
@@ -165,37 +166,46 @@ void verify(Registry r) {
           impl.fn(data.input, data.threshold, data.padding_h, data.padding_w,
                   data.kernel, data.stride_h, data.stride_w, data.relu_alpha);
 
-      // Data for the direct convolution is always provided in NCHW shape
+      // Data for the direct convolution is always provided in NCHW shape. Thus,
+      // we need to reshape when the input is generated in NHWC. The input is
+      // already generated in the appropriate shape to make the benchmarking
+      // faster.
       // @all: I had to separate this in an if-then-else otherwise C++
       // continues complaining (it's not happy if I use a ternary operator)
+      Tensor4D<float> reshaped_input(data.batch_size, data.channels,
+                                     data.input_h, data.input_w, false);
+      if (data.data_order == DataOrder::NHWC) {
+        Tensor4D<float> reshaped = reshape_nhwc_nchw(data.input);
+        assert(reshaped.dim1 == reshaped_input.dim1);
+        assert(reshaped.dim2 == reshaped_input.dim2);
+        assert(reshaped.dim3 == reshaped_input.dim3);
+        assert(reshaped.dim4 == reshaped_input.dim4);
+        memcpy(reshaped_input.data, reshaped.data,
+               sizeof(float) * data.input.dim1 * data.input.dim2 *
+                   data.input.dim3 * data.input.dim4);
+      } else {
+        assert(data.input.dim1 == reshaped_input.dim1);
+        assert(data.input.dim2 == reshaped_input.dim2);
+        assert(data.input.dim3 == reshaped_input.dim3);
+        assert(data.input.dim4 == reshaped_input.dim4);
+        memcpy(reshaped_input.data, data.input.data,
+               sizeof(float) * data.input.dim1 * data.input.dim2 *
+                   data.input.dim3 * data.input.dim4);
+      }
+      Tensor4D<float> padded_input =
+          direct_pad(reshaped_input, data.padding_h, data.padding_w);
       Tensor4D<float> reference_output(output.dim1, output.dim2, output.dim3,
                                        output.dim4, false);
-      if (data.data_order == DataOrder::NCHW) {
-        Tensor4D<float> padded_input =
-            direct_pad(data.input, data.padding_h, data.padding_w);
-        std::vector<float> ref_out =
-            direct_conv(padded_input.data, data.real_kernel.data, data.stride_h,
-                        data.stride_w, padded_input.dim1, padded_input.dim2,
-                        padded_input.dim3, padded_input.dim4, data.kernel_n,
-                        data.kernel_h, data.kernel_w);
+      std::vector<float> ref_out =
+          direct_conv(padded_input.data, data.real_kernel.data, data.stride_h,
+                      data.stride_w, padded_input.dim1, padded_input.dim2,
+                      padded_input.dim3, padded_input.dim4, data.kernel_n,
+                      data.kernel_h, data.kernel_w);
 
-        assert(ref_out.size() ==
-               output.dim1 * output.dim2 * output.dim3 * output.dim4);
-        for (size_t i = 0; i < ref_out.size(); ++i)
-          reference_output.data[i] = ref_out[i];
-      } else {
-        Tensor4D<float> padded_input = direct_pad(
-            reshape_nhwc_nchw(data.input), data.padding_h, data.padding_w);
-        std::vector<float> ref_out =
-            direct_conv(padded_input.data, data.real_kernel.data, data.stride_h,
-                        data.stride_w, padded_input.dim1, padded_input.dim2,
-                        padded_input.dim3, padded_input.dim4, data.kernel_n,
-                        data.kernel_h, data.kernel_w);
-        assert(ref_out.size() ==
-               output.dim1 * output.dim2 * output.dim3 * output.dim4);
-        for (size_t i = 0; i < ref_out.size(); ++i)
-          reference_output.data[i] = ref_out[i];
-      }
+      assert(ref_out.size() ==
+             output.dim1 * output.dim2 * output.dim3 * output.dim4);
+      memcpy(reference_output.data, ref_out.data(),
+             sizeof(float) * ref_out.size());
 
       // Compare the conv results to ensure the functions are correct
       int cmp;
