0001e434bd834ce8adfe7ce09499ff3b8343cec2
rpeterson <rudypotamia@gmail.com>
2024-06-21 13:11:43 +0200

    input_size in ternary

--- plotting/impls/best_impl_avx2.py
+++ plotting/impls/best_impl_avx2.py
@@ -27,66 +27,66 @@ class BestImplAVX2(T2RGemmLU):
         vec_loops = CNTBITS // 16
 
         # _mm256_cmp_ps(current_values01, curr_thresh_avx, _CMP_GT_OS));
-        flops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 8
+        flops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 8
 
         # _mm256_cmp_ps(current_values01, neg_curr_thresh_avx, _CMP_LT_OS)
-        flops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 8
+        flops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 8
 
         # _mm256_or_si256(current_values01_gt_thresh, current_values01_lt_neg_thresh);
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 8
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 8
 
         # _mm256_cmp_ps(current_values23, curr_thresh_avx, _CMP_GT_OS));
-        flops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 8
+        flops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 8
 
         # _mm256_cmp_ps(current_values23, neg_curr_thresh_avx, _CMP_LT_OS)
-        flops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 8
+        flops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 8
 
         # _mm256_or_si256(current_values23_gt_thresh, current_values23_lt_neg_thresh);
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 8
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 8
 
         # TODO: I am not sure how many ops _mm256_cvtepi32_epi64 is...?
         # I will assume a sign-extending a single integer is one iop.
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 4
 
         # _mm256_or_si256(first_bits0, one_bits0)
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 4
         # _mm256_or_si256(first_bits1, one_bits1)
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 4
         # _mm256_or_si256(first_bits2, one_bits2)
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 4
         # _mm256_or_si256(first_bits3, one_bits3)
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 4
 
         # TODO: I am not sure how many ops _mm256_cvtepi32_epi64 is...?
         # I will assume a sign-extending a single integer is one iop.
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 4
 
         # _mm256_or_si256(second_bits0, one_bits0)
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 4
         # _mm256_or_si256(second_bits1, one_bits1)
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 4
         # _mm256_or_si256(second_bits2, one_bits2)
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 4
         # _mm256_or_si256(second_bits3, one_bits3)
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 4
 
         # _mm256_or_si256(first_bits0, _mm256_or_si256(first_bits1, _mm256_or_si256(first_bits2, first_bits3)));
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * 3 * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * 3 * 4
 
         # _mm256_or_si256(first_bits_ored, first_bits_ored_perm)
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * 4
 
         # _mm256_or_si256(second_bits0, _mm256_or_si256(second_bits1, _mm256_or_si256(second_bits2, second_bits3)));
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * 3 * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * 3 * 4
 
         # _mm256_or_si256(second_bits_ored, second_bits_ored_perm)
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * 4
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * 4
 
         # first_bits |= first_bits_0 | first_bits_1
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * 2
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * 2
 
         # second_bits |= second_bits_0 | second_bits_1
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * 2
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * 2
 
         iops = ceil(iops)
         flops = ceil(flops)
--- plotting/impls/best_impl_avx512.py
+++ plotting/impls/best_impl_avx512.py
@@ -26,49 +26,49 @@ class BestImplAVX512(T2RGemmLU):
         vec_loops = CNTBITS // 16
 
         # _mm512_cmp_ps_mask(current_values, curr_thresh_avx512, _CMP_GT_OS)
-        flops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 16
+        flops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 16
 
         # _mm512_cmp_ps_mask(current_values, neg_curr_thresh_avx512, _CMP_LT_OS)
-        flops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 16
+        flops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 16
 
         # __mmask16 current_values_gt_or_lt = current_values_gt_thresh | current_values_lt_neg_thresh;
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops
 
         # __mmask8 lower_first_bits_mask = (__mmask8)(current_values_lt_neg_thresh & 0xFF);
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops
 
         # __mmask8 lower_second_bits_mask = (__mmask8)(current_values_gt_or_lt & 0xFF);
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops
 
         # __mmask8 upper_first_bits_mask = (__mmask8)(current_values_lt_neg_thresh >> 8);
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops
 
         # __mmask8 upper_second_bits_mask = (__mmask8)(current_values_gt_or_lt >> 8);
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops
 
         # first_bits_lower = _mm512_mask_or_epi64(first_bits_lower, lower_first_bits_mask, first_bits_lower, one_bit_lower);
-        iops += .5 * self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 8
+        iops += .5 * self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 8
 
         # first_bits_upper = _mm512_mask_or_epi64(first_bits_upper, upper_first_bits_mask, first_bits_upper, one_bit_upper);
-        iops += .5 * self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 8
+        iops += .5 * self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 8
 
         # second_bits_lower = _mm512_mask_or_epi64(second_bits_lower, lower_second_bits_mask, second_bits_lower, one_bit_lower);
-        iops += .5 * self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 8
+        iops += .5 * self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 8
 
         # second_bits_upper = _mm512_mask_or_epi64(second_bits_upper, upper_second_bits_mask, second_bits_upper, one_bit_upper);
-        iops += .5 * self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * vec_loops * 8
+        iops += .5 * self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * vec_loops * 8
 
         # first_bits = _mm512_reduce_or_epi64(_mm512_or_epi64(first_bits_lower, first_bits_upper));
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * 8 * 7
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * 8 * 7
 
         # second_bits = _mm512_reduce_or_epi64(_mm512_or_epi64(second_bits_lower, second_bits_upper));
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * 8 * 7
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * 8 * 7
 
         # first_bits |= first_bits0 | first_bits1;
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * 2
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * 2
 
         # second_bits |= second_bits0 | second_bits1;
-        iops += self.p.batch_size * self.p.kernel_height * self.p.kernel_width * self.pri_channels * 2
+        iops += self.p.batch_size * self.p.input_height * self.p.input_width * self.pri_channels * 2
 
         iops = ceil(iops)
         flops = ceil(flops)
